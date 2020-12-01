2c6c1c508596b5aaf757693830d84565c773ac29
menachem m margolis <margolismendel5728@gmail.com>
2020-12-01 18:17:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,20 +5,20 @@ def key_for_min_value(name_hash)
 
 
    sv = nil
-  
-   
+
+
    name_hash.each do|k,v|
-    
+
      if sv == nil
        sv = v
      end
-     if v < sv 
-       sv = v 
-     end 
-     if v == sv
-      return k 
+     if v < sv
+       sv = v
      end
- 
+     if v == sv
+      return k
+     end
+
 
   end
  end
